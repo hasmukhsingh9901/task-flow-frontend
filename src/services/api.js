456a31f3c4fee9c01cdc5ba128136000189c860a
hasmukhsingh9901 <hasmukhsingh90@gmail.com>
2025-07-30 14:26:33 +0530

    removed

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://task-flow-backend-3816.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 
 // Create axios instance with default config
 const api = axios.create({
